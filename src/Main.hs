900e41c8d0765edcf2a513d8b2069ca8864df9eb
Carlos Gustavo Ruiz <atmantree@gmail.com>
2018-04-12 13:00:38 -0500

    initial args parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,32 @@
 module Main where
 
+import System.Environment (getArgs)
+
+showTasks :: Bool -> IO ()
+showTasks currentOnly = if currentOnly == True then
+                           putStrLn "TODO: show current tasks"
+                         else
+                           putStrLn "TODO: show all tasks"
+
+setTaskProgress :: Integer -> IO ()
+setTaskProgress progress = putStrLn "TODO: set task progress"
+
+addNewTask :: [String] -> IO ()
+addNewTask task = putStrLn "TODO: add new task"
+
+deleteTask :: Integer -> IO ()
+deleteTask taskId = putStrLn "TODO: remove task from list"
+
+dailyTasks args
+  | args == []            = showTasks True
+  | head args == "all"    = showTasks False
+  | head args == "add"    = addNewTask $ tail args
+  | head args == "update" = setTaskProgress 0
+  | head args == "del"    = deleteTask 0 
+  | otherwise             = putStrLn "TODO: add usage message"
+
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  args <- getArgs
+  dailyTasks args
